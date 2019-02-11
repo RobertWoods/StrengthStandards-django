587a3f63ef4eda243cc5c4ad4d4ff30130ec98da
Robert Woods <robertwoods2013@gmail.com>
2019-02-10 19:11:23 -0500

    init, has model for PRs

---
+++ strength_standards/strength_standards/quickstart/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from strength_standards.quickstart.models import PersonalRecord
+# Register your models here.
+admin.site.register(PersonalRecord)
---
+++ strength_standards/strength_standards/quickstart/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
+# Create your models here.
+class PersonalRecord(models.Model):
+    name = models.CharField(max_length=50)
+    weight = models.FloatField()
+    reps = models.IntegerField(default=1)
+    date = models.DateField(default=date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
